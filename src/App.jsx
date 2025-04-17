ef313d7893e3ede6b38dc184a5fc4745d66e0704
Eko <ekodreshaj@mac.mynetworksettings.com>
2025-04-16 22:44:30 -0400

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
 // src/App.jsx
-import { useState } from 'react';
-import './App.css';
-import IngredientList from './components/IngredientList/IngredientList.jsx';
-import BurgerStack from './components/BurgerStack/BurgerStack.jsx';
+import { useState } from "react";
+import "./App.css";
+import IngredientList from "./components/IngredientList/IngredientList.jsx";
+import BurgerStack from "./components/BurgerStack/BurgerStack.jsx";
 
 const App = () => {
   const availableIngredients = [
-    { name: 'Kaiser Bun', color: 'saddlebrown' },
-    { name: 'Sesame Bun', color: 'sandybrown' },
-    { name: 'Gluten Free Bun', color: 'peru' },
-    { name: 'Lettuce Wrap', color: 'olivedrab' },
-    { name: 'Beef Patty', color: '#3F250B' },
-    { name: 'Soy Patty', color: '#3F250B' },
-    { name: 'Black Bean Patty', color: '#3F250B' },
-    { name: 'Chicken Patty', color: 'burlywood' },
-    { name: 'Lettuce', color: 'lawngreen' },
-    { name: 'Tomato', color: 'tomato' },
-    { name: 'Bacon', color: 'maroon' },
-    { name: 'Onion', color: 'lightyellow' },
-    { name: 'Cheddar Cheese', color: '#FDE18B' },
-    { name: 'Swiss Cheese', color: '#F1E1A8' },
+    { name: "Kaiser Bun", color: "saddlebrown" },
+    { name: "Sesame Bun", color: "sandybrown" },
+    { name: "Gluten Free Bun", color: "peru" },
+    { name: "Lettuce Wrap", color: "olivedrab" },
+    { name: "Beef Patty", color: "#3F250B" },
+    { name: "Soy Patty", color: "#3F250B" },
+    { name: "Black Bean Patty", color: "#3F250B" },
+    { name: "Chicken Patty", color: "burlywood" },
+    { name: "Lettuce", color: "lawngreen" },
+    { name: "Tomato", color: "tomato" },
+    { name: "Bacon", color: "maroon" },
+    { name: "Onion", color: "lightyellow" },
+    { name: "Cheddar Cheese", color: "#FDE18B" },
+    { name: "Swiss Cheese", color: "#F1E1A8" },
   ];
 
-  const [stack, setStack] = useState([]); // 👈 your burger stack state!
+  const [stack, setStack] = useState([]); //   burger stack state!
 
   return (
     <main>
       <h1>Burger Stacker</h1>
-      <section>
-        <IngredientList ingredients={availableIngredients} setStack={setStack} stack={stack} />
+      <section
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "400px",
+        }}
+      >
+        <IngredientList
+          ingredients={availableIngredients}
+          setStack={setStack}
+          stack={stack}
+        />
         <BurgerStack stack={stack} setStack={setStack} />
       </section>
     </main>
-);
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'; // optional in modern React, but still nice
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react"; // optional in modern React, but still nice
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
